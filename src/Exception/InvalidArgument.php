25ec645ebb5e456920b647213890e73ae963dc66
George Steel <george@net-glue.co.uk>
2020-06-02 00:04:35 +0100

    Remove unused method

--- src/Exception/InvalidArgument.php
+++ src/Exception/InvalidArgument.php
@@ -5,7 +5,6 @@ namespace Prismic\Exception;
 
 use InvalidArgumentException;
 use Prismic\Json;
-use Prismic\ResultSet;
 use Prismic\Value\FormField;
 use Psr\Http\Message\UriInterface;
 use function get_class;
@@ -86,14 +85,5 @@ class InvalidArgument extends InvalidArgumentException implements PrismicError
             $previewUri->getHost(),
             $apiUri->getHost()
         ));
-    }
-
-    public static function invalidResultSetClass(string $resultSetClass) : self
-    {
-        return new static(
-            'The custom result set type "%s" does not implement %s',
-            $resultSetClass,
-            ResultSet::class
-        );
     }
 }
