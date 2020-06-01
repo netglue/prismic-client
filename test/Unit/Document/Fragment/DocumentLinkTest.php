b62d1b0d1d7fcc3bb614fe39be480715c4d9efc2
George Steel <george@net-glue.co.uk>
2020-06-01 14:26:25 +0100

    Add unit test for document links

---
+++ test/Unit/Document/Fragment/DocumentLinkTest.php
@@ -0,0 +1,90 @@
+<?php
+declare(strict_types=1);
+
+namespace PrismicTest\Document\Fragment;
+
+use Prismic\Document\Fragment\DocumentLink;
+use PrismicTest\Framework\TestCase;
+use TypeError;
+
+class DocumentLinkTest extends TestCase
+{
+    public function testItIsATypeErrorForATagToBeANonString() : void
+    {
+        $this->expectException(TypeError::class);
+        DocumentLink::new(
+            'id',
+            'uid',
+            'type',
+            'en-gb',
+            'slime',
+            false,
+            [1,2],
+        );
+    }
+
+    public function testConstructor() : DocumentLink
+    {
+        $link = DocumentLink::new(
+            'id',
+            'uid',
+            'type',
+            'en-gb',
+            'slime',
+            false,
+            ['a','b'],
+        );
+        $this->addToAssertionCount(1);
+
+        return $link;
+    }
+
+    /** @depends testConstructor */
+    public function testThatIdIsExpectedValue(DocumentLink $link) : void
+    {
+        $this->assertSame('id', $link->id());
+    }
+
+    /** @depends testConstructor */
+    public function testThatUidIsExpectedValue(DocumentLink $link) : void
+    {
+        $this->assertSame('uid', $link->uid());
+    }
+
+    /** @depends testConstructor */
+    public function testThatTypeIsExpectedValue(DocumentLink $link) : void
+    {
+        $this->assertSame('type', $link->type());
+    }
+
+    /** @depends testConstructor */
+    public function testThatLanguageIsExpectedValue(DocumentLink $link) : void
+    {
+        $this->assertSame('en-gb', $link->language());
+    }
+
+    /** @depends testConstructor */
+    public function testThatSlugIsExpectedValue(DocumentLink $link) : void
+    {
+        $this->assertSame('slime', $link->slug());
+    }
+
+    /** @depends testConstructor */
+    public function testThatIsBrokenIsExpectedValue(DocumentLink $link) : void
+    {
+        $this->assertFalse($link->isBroken());
+    }
+
+    /** @depends testConstructor */
+    public function testThatALinkIsNotConsideredEmpty(DocumentLink $link) : void
+    {
+        $this->assertFalse($link->isEmpty());
+    }
+
+    /** @depends testConstructor */
+    public function testThatTagsHaveExpectedMembers(DocumentLink $link) : void
+    {
+        $this->assertContainsEquals('a', $link->tags());
+        $this->assertContainsEquals('b', $link->tags());
+    }
+}
