ccb950a70dc4d81ec45b81c6bc1ae1ce571ec26e
George Steel <george@net-glue.co.uk>
2020-06-04 20:43:23 +0100

    Add ApiClient interface so that it's easier for consumers to stub out the api client when testing, or replacing the implementation?

--- src/Api.php
+++ src/Api.php
@@ -24,25 +24,8 @@ use function sprintf;
 use function str_replace;
 use function urldecode;
 
-final class Api
+final class Api implements ApiClient
 {
-    /**
-     * The default form/collection name to query for results
-     */
-    public const DEFAULT_FORM = 'everything';
-
-    /**
-     * Name of the cookie that will be used to remember the preview reference
-     */
-    public const PREVIEW_COOKIE = 'io.prismic.preview';
-
-    /**
-     * Name of the cookie that will be used to remember the experiment reference
-     *
-     * This constant is currently unused because it is no longer possible to run A/B tests with Prismic
-     */
-    public const EXPERIMENTS_COOKIE = 'io.prismic.experiment';
-
     /** @var ClientInterface */
     private $httpClient;
 
@@ -167,12 +150,6 @@ final class Api
         return $response;
     }
 
-    /**
-     * Return the current ref in use
-     *
-     * By default, this is the master ref. Super-global cookies are consulted to detect whether a preview
-     * session is active and if so, the preview ref will be preferred.
-     */
     public function ref() : Ref
     {
         $ref = $this->previewRef();
@@ -183,20 +160,12 @@ final class Api
         return $this->data()->master();
     }
 
-    /**
-     * Create a new query on the current ref
-     *
-     * Use the query to build your conditions then dispatch it to @link query() to retrieve the results.
-     */
     public function createQuery(string $form = self::DEFAULT_FORM) : Query
     {
         return (new Query($this->data()->form($form)))
             ->ref($this->ref());
     }
 
-    /**
-     * Submit the given query to the API
-     */
     public function query(Query $query) : ResultSet
     {
         return $this->resultSetFactory->withHttpResponse($this->sendRequest(
@@ -204,17 +173,11 @@ final class Api
         ));
     }
 
-    /**
-     * Convenience method to return the first document for the given query
-     */
     public function queryFirst(Query $query) :? Document
     {
         return $this->query($query)->first();
     }
 
-    /**
-     * Locate a single document by its unique identifier
-     */
     public function findById(string $id) :? Document
     {
         $query = $this->createQuery()
@@ -224,9 +187,6 @@ final class Api
         return $this->queryFirst($query);
     }
 
-    /**
-     * Locate a single document by its type and user unique id
-     */
     public function findByUid(string $type, string $uid, string $lang = '*') :? Document
     {
         $path = sprintf('my.%s.uid', $type);
@@ -237,9 +197,6 @@ final class Api
         return $this->queryFirst($query);
     }
 
-    /**
-     * Locate the document referenced by the given bookmark
-     */
     public function findByBookmark(string $bookmark) :? Document
     {
         return $this->findById($this->data()->bookmark($bookmark)->documentId());
@@ -255,14 +212,7 @@ final class Api
         return $uri->withQuery(http_build_query($params));
     }
 
-    /**
-     * Set cookie values found in the request
-     *
-     * If preview and experiment cookie values are not available in your environment in the $_COOKIE super global, you
-     * can provide them here and they'll be inspected to see if a preview is required or an experiment is running
-     *
-     * @param string[] $cookies
-     */
+    /** @inheritDoc */
     public function setRequestCookies(array $cookies) : void
     {
         $this->requestCookies = $cookies;
@@ -327,12 +277,6 @@ final class Api
         return $uri;
     }
 
-    /**
-     * Start a preview session
-     *
-     * If the preview session can be resolved to a single relevant document, this method will return a document link
-     * for that document with which you can construct a url using your {@link LinkResolver} to redirect the user to.
-     */
     public function previewSession(string $token) :? DocumentLink
     {
         $uri = $this->validatePreviewToken($token);
---
+++ src/ApiClient.php
@@ -0,0 +1,112 @@
+<?php
+declare(strict_types=1);
+
+namespace Prismic;
+
+use Prismic\Document\Fragment\DocumentLink;
+use Prismic\Value\ApiData;
+use Prismic\Value\Ref;
+
+interface ApiClient
+{
+    /**
+     * The default form/collection name to query for results
+     */
+    public const DEFAULT_FORM = 'everything';
+
+    /**
+     * Name of the cookie that will be used to remember the preview reference
+     */
+    public const PREVIEW_COOKIE = 'io.prismic.preview';
+
+    /**
+     * Name of the cookie that will be used to remember the experiment reference
+     *
+     * This constant is currently unused because it is no longer possible to run A/B tests with Prismic
+     */
+    public const EXPERIMENTS_COOKIE = 'io.prismic.experiment';
+
+    /** Return the host name of the api endpoint */
+    public function host() : string;
+
+    /** Returns a value object containing current information about the content repository */
+    public function data() : ApiData;
+
+    /**
+     * Return the current ref in use
+     *
+     * By default, this is the master ref. Super-global cookies are consulted to detect whether a preview
+     * session is active and if so, the preview ref will be preferred.
+     */
+    public function ref() : Ref;
+
+    /**
+     * Create a new query on the current ref
+     *
+     * Use the query to build your conditions then dispatch it to @link query() to retrieve the results.
+     */
+    public function createQuery(string $form = self::DEFAULT_FORM) : Query;
+
+    /**
+     * Submit the given query to the API
+     */
+    public function query(Query $query) : ResultSet;
+
+    /**
+     * Convenience method to return the first document for the given query
+     */
+    public function queryFirst(Query $query) :? Document;
+
+    /**
+     * Locate a single document by its unique identifier
+     */
+    public function findById(string $id) :? Document;
+
+    /**
+     * Locate a single document by its type and user unique id
+     */
+    public function findByUid(string $type, string $uid, string $lang = '*') :? Document;
+
+    /**
+     * Locate the document referenced by the given bookmark
+     */
+    public function findByBookmark(string $bookmark) :? Document;
+
+    /**
+     * Return a result set containing all, un-paginated results for the given query
+     */
+    public function findAll(Query $query) : ResultSet;
+
+    /**
+     * Given a paginated result, return the next page of the results, if any
+     */
+    public function next(ResultSet $resultSet) :? ResultSet;
+
+    /**
+     * Given a paginated result, return the previous page of the results, if any
+     */
+    public function previous(ResultSet $resultSet) :? ResultSet;
+
+    /**
+     * Set cookie values found in the request
+     *
+     * If preview and experiment cookie values are not available in your environment in the $_COOKIE super global, you
+     * can provide them here and they'll be inspected to see if a preview is required or an experiment is running
+     *
+     * @param string[] $cookies
+     */
+    public function setRequestCookies(array $cookies) : void;
+
+    /**
+     * Whether the current ref in use is a preview, i.e. the user is in preview mode
+     */
+    public function inPreview() : bool;
+
+    /**
+     * Start a preview session
+     *
+     * If the preview session can be resolved to a single relevant document, this method will return a document link
+     * for that document with which you can construct a url using your {@link LinkResolver} to redirect the user to.
+     */
+    public function previewSession(string $token) :? DocumentLink;
+}
