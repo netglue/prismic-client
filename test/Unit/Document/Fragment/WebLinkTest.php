07b534110921b40bf88b66bbb0c9b1ad194c4f40
George Steel <george@net-glue.co.uk>
2020-06-18 14:28:36 +0100

    Cover some low hanging fruit to make Codecov checks pass

---
+++ test/Unit/Document/Fragment/WebLinkTest.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace PrismicTest\Document\Fragment;
+
+use Prismic\Document\Fragment\WebLink;
+use PrismicTest\Framework\TestCase;
+
+class WebLinkTest extends TestCase
+{
+    public function testAccessors() : void
+    {
+        $link = WebLink::new('somewhere', 'target');
+        $this->assertSame('somewhere', $link->url());
+        $this->assertSame('target', $link->target());
+    }
+
+    public function testThatCastingToStringYieldsTheUrl() : void
+    {
+        $link = WebLink::new('somewhere', 'target');
+        $this->assertSame('somewhere', (string) $link);
+    }
+
+    public function testThatWebLinksAreNotConsideredEmpty() : void
+    {
+        $link = WebLink::new('somewhere', 'target');
+        $this->assertFalse($link->isEmpty());
+    }
+}
