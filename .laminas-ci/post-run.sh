183feff613e7004197c24ebdd59f1a34522e341e
George Steel <george@net-glue.co.uk>
2021-03-15 22:55:03 +0000

    What's up with the ini file?

--- .laminas-ci/post-run.sh
+++ .laminas-ci/post-run.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
+
+# $1 = Exit Status of Job
+# $2 = User
+# $3 = WorkDir
+# $4 = The JOB Json String
+
+cat /etc/php/"${PHP}"/cli/conf.d/99-settings.ini
+
 bash <(curl -s https://codecov.io/bash)
