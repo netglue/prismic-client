cb7f65d060566d67c12a573dbbb0c005ee08bec1
George Steel <george@net-glue.co.uk>
2021-04-19 12:25:24 +0100

    Investigate appending coverage action after each QA check (#21)
    Switch to Laminas CI Setup with additional configuration for processing coverage

--- .laminas-ci/post-run.sh
+++ .laminas-ci/post-run.sh
@@ -1,2 +1,13 @@
 #!/usr/bin/env bash
+
+# $1 = Exit Status of Job
+# $2 = User
+# $3 = WorkDir
+# $4 = The JOB Json String
+
+JOB=$4
+PHP_VERSION=$(echo "${JOB}" | jq -r '.php')
+
+cat /etc/php/"$PHP_VERSION"/cli/conf.d/99-settings.ini
+
 bash <(curl -s https://codecov.io/bash)
