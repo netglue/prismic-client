17c76e1fdbedcba4086591606c16f1ef06704182
George Steel <george@net-glue.co.uk>
2021-08-24 16:56:57 +0100

    Try with pecl again…

---
+++ .laminas-ci/pre-install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+# $1: the user the QA command will run under
+# $2: the WORKDIR path
+# $3: the $JOB passed to the entrypoint (see above)
+
+PHP_VERSION=$(echo "${JOB}" | jq -r '.php')
+pecl install php"$PHP_VERSION"-pcov
+
+phpdismod -v ALL -s ALL xdebug
+phpenmod -v ALL -s ALL pcov
--- .laminas-ci/pre-run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# disable Xdebug and enable pcov:
-
-pecl install pcov
-
-phpdismod -v ALL -s ALL xdebug
-phpenmod -v ALL -s ALL pcov
