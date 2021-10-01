c79c2030b92dafdbdf5dc185e09b2a7b4a4b0614
George Steel <george@net-glue.co.uk>
2021-10-01 12:09:11 +0100

    Fixes #56

--- test/Unit/Exception/PreviewTokenExpiredTest.php
+++ test/Unit/Exception/PreviewTokenExpiredTest.php
@@ -11,6 +11,26 @@ use Psr\Http\Message\RequestInterface;
 
 class PreviewTokenExpiredTest extends TestCase
 {
+    /** @return array<string, array{0: string[], 1:int}> */
+    public function possibleResponseBodiesThatShouldRepresentExpiredPreviews(): array
+    {
+        return [
+            'Legacy version of error message' => [
+                ['error' => 'Preview token expired'],
+                400,
+            ],
+            'Error message observed mid to late 2021' => [
+                [
+                    'type' => 'api_security_error',
+                    'message' => 'This preview token has expired',
+                    'oauth_initiate' => 'https://something.prismic.io/auth',
+                    'oauth_token' => 'https://something.prismic.io/auth/token',
+                ],
+                404,
+            ],
+        ];
+    }
+
     public function testIsPreviewTokenExpiryIsFalseWhenErrorMessageDoesNotMatchExpectedValue(): void
     {
         $response = new JsonResponse(['error' => 'Not expected message']);
@@ -41,5 +61,16 @@ class PreviewTokenExpiredTest extends TestCase
         $this->assertSame(400, $error->getCode());
         $this->assertSame($request, $error->getRequest());
         $this->assertSame($response, $error->getResponse());
+    }
+
+    /**
+     * @param array<string, string> $bodyPayload
+     *
+     * @dataProvider possibleResponseBodiesThatShouldRepresentExpiredPreviews
+     */
+    public function testVariousResponsesWillBeConsideredTokenExpiryConditions(array $bodyPayload, int $responseCode): void
+    {
+        $response = new JsonResponse($bodyPayload, $responseCode);
+        self::assertTrue(PreviewTokenExpired::isPreviewTokenExpiry($response));
     }
 }
